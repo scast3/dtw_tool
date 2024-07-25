9fae4f42299a1d873237c40072898b9272f3d0ca
santi <santiagoc333@gmail.com>
2024-07-25 13:21:01 -0300

    completed calculation, need to work on visualization now

--- marker_functions.py
+++ marker_functions.py
@@ -8,16 +8,17 @@ def create_subs(arr):
     
     # Add pairs of consecutive elements
     for i in range(len(arr) - 1):
-        result.append([arr[i], arr[i + 1]])
+        result.append((arr[i], arr[i + 1]))
     
     # Add the final pair with infinity
     if len(arr) > 0:
-        result.append([arr[-1], float('inf')])
+        result.append((arr[-1], float('inf')))
     
     return result
 
 def mkr_calc_window(G, node_name1, node_name2, show_plot = False):
-    mkr_df = pd.DataFrame(columns=["Label", f"Range {node_name1}", f"Range {node_name2}", "Error"])
+    mkr_df = pd.DataFrame(columns=["Label", f"Range {node_name1}", f"Range {node_name2}", 
+                                   f"Opt Win {node_name1}", f"Opt Win {node_name2}", "Error"])
 
     mark1 = G.nodes[node_name1]["markers"].copy()
     mark2 = G.nodes[node_name2]["markers"].copy()
@@ -45,12 +46,12 @@ def mkr_calc_window(G, node_name1, node_name2, show_plot = False):
     sub1 = create_subs(mark_depths1)
     sub2 = create_subs(mark_depths2)
 
-    print(sub1)
-    print(sub2)
-
     # run the optimization for the narrowed marker window
     m_tol = 5
     for i, label in enumerate(labels):
+        mkr_df.at[i, f"Range {node_name1}"] = sub1[i]
+        mkr_df.at[i, f"Range {node_name2}"] = sub2[i]
+
         print(f"Calculating windows for marker range {label}")
         bounds = [(sub1[i][0]-m_tol, sub1[i][0]+m_tol),
                 (sub1[i][1]-m_tol, sub1[i][1]+m_tol),
@@ -62,6 +63,11 @@ def mkr_calc_window(G, node_name1, node_name2, show_plot = False):
 
         print(f"Optimal windows: {opt}")
         print(f"Minimum error: {error}")
+
+        mkr_df.at[i, "Error"] = error
+        mkr_df.at[i, f"Opt Win {node_name1}"] = (opt[0],opt[1])
+        mkr_df.at[i, f"Opt Win {node_name2}"] = (opt[2],opt[3])
+
 
     if show_plot:
         print("lol!")
