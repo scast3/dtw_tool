59bc10ae10cd45e968e2a2107d2ff8c7fe36e3cf
santi <santiagoc333@gmail.com>
2024-08-01 12:20:33 -0300

    comments in markers script

--- marker_functions.py
+++ marker_functions.py
@@ -1,6 +1,20 @@
+"""
+Script Name: marker_functions.py
+Description: Funciones para crear ventanas usando marcadores
+
+Author: Santiago Castillo
+Date Created: 24/7/2024
+Last Modified: 1/8/2024
+Version: 1.0
+
+Dependencies:
+    - pandas (pip install pandas)
+"""
+
 import pandas as pd
 from calc_functions import optimal_window
 
+# helper method
 def create_subs(arr):
     result = [(0, arr[0])]
     
@@ -15,6 +29,41 @@ def create_subs(arr):
     return result
 
 def mkr_calc_window(G, node_name1, node_name2, show_ranges = False, show_error = False):
+    """
+    Calcula las ventanas óptimas de marcadores para dos nodos en un grafo, considerando el rango de profundidad de 
+    los marcadores comunes entre los nodos.
+
+    Parámetros:
+    G : networkx.Graph
+        El grafo que contiene los nodos con los datos de marcadores.
+    node_name1 : str
+        El nombre del nodo referencia en el grafo.
+    node_name2 : str
+        El nombre del segundo nodo en el grafo.
+    show_ranges : bool, opcional
+        Si es True, se mostrará el rango de marcadores (por defecto es False).
+    show_error : bool, opcional
+        Si es True, se mostrará el error asociado a las ventanas óptimas (por defecto es False).
+
+    Retorna:
+    pandas.DataFrame
+        Un DataFrame con las siguientes columnas:
+        - "Label": Etiquetas de los rangos de marcadores.
+        - f"Range {node_name1}": Rango de profundidad del label para el primer nodo.
+        - f"Range {node_name2}": Rango de profundidad del label para el segundo nodo.
+        - f"Opt Win {node_name1}": Ventana óptima para el primer nodo.
+        - f"Opt Win {node_name2}": Ventana óptima para el segundo nodo.
+        - "Error": Error asociado a la ventana óptima.
+
+    Ejemplo:
+    mkr_calc_window(G, "Nodo1", "Nodo2", show_ranges=True, show_error=True)
+
+    Notas:
+    - Asegúrese de que ambos nodos tengan la clave "markers" que contenga los datos de marcadores y profundidades.
+    - La función crea rangos de profundidad basados en los marcadores comunes entre los dos nodos y calcula 
+      las ventanas óptimas y el error asociado.
+    """
+
     mkr_df = pd.DataFrame(columns=["Label", f"Range {node_name1}", f"Range {node_name2}", 
                                    f"Opt Win {node_name1}", f"Opt Win {node_name2}", "Error"])
 
@@ -45,7 +94,7 @@ def mkr_calc_window(G, node_name1, node_name2, show_ranges = False, show_error =
     sub2 = create_subs(mark_depths2)
 
     # run the optimization for the narrowed marker window
-    m_tol = 5
+    m_tol = 5 # +/- bounds based on marker depth
     for i, label in enumerate(labels):
         mkr_df.at[i, f"Range {node_name1}"] = sub1[i]
         mkr_df.at[i, f"Range {node_name2}"] = sub2[i]
@@ -66,12 +115,12 @@ def mkr_calc_window(G, node_name1, node_name2, show_ranges = False, show_error =
         mkr_df.at[i, f"Opt Win {node_name1}"] = (opt[0],opt[1])
         mkr_df.at[i, f"Opt Win {node_name2}"] = (opt[2],opt[3])
 
-
+    # need to implement later
     if show_ranges:
-        print("lol!")
+        print("show ranges")
 
     if show_error:
-        print("err")
+        print("show error")
 
     return mkr_df
 
